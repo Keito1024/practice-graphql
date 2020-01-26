a8e7282fe1dc44994c258554fc69babe2789cab5
Keito1024 <kkrt0724@gmail.com>
2020-01-27 01:32:53 +0900

    query叩いてみる

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,56 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ApolloClient, { gql } from "apollo-boost";
+
+
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  request: operation => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${
+          process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+        }`
+      }
+    });
+  }
+});
+
+const query = gql`
+  {
+    org1: organization(login: "apollographql") {
+      repositories(first: 5) {
+        nodes {
+          ...commonFields
+        }
+      }
+    },
+    org2: organization(login: "teamlab") {
+      repositories(first: 5) {
+        nodes {
+          ...commonFields
+        }
+      }
+    }
+  }
+fragment commonFields on Repository {
+  id
+  name
+  url
+  viewerHasStarred
+  stargazers {
+    totalCount
+  }
+}
+`
+
+// queryを叩いてみる
+client.query({
+  query
+}).then(result => {
+  console.log(result)
+})
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
